f3f67e38dd5844c547fa6f2c5ea1e1e3563ace65
CALL ME BIG SHARK! <34434247+lrzh0123@users.noreply.github.com>
2020-05-31 22:08:16 +0800

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+  int a,b;
+  a=0;
+  b=1;
+  cout<<a+b<<endl;
+  cout<<"My First repository"<<endl;
+  cout<<"Hello World!"<<endl;
+  return 0;
+}
